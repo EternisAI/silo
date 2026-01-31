a74a6c132e7a10bec9009fccb7425861963d8555
Jason Lee <lwlee2608@gmail.com>
2026-02-01 01:29:48 +0800

    fix preflight port check

--- internal/installer/preflight.go
+++ internal/installer/preflight.go
@@ -1,8 +1,10 @@
 package installer
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"os"
 	"syscall"
 
 	"github.com/eternisai/silo/internal/docker"
@@ -35,6 +37,18 @@ func CheckPortAvailability(port int) error {
 	addr := fmt.Sprintf(":%d", port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
+		// Check if it's a permission error (common for ports < 1024 on Linux)
+		var opErr *net.OpError
+		if errors.As(err, &opErr) {
+			var syscallErr *os.SyscallError
+			if errors.As(opErr.Err, &syscallErr) {
+				if errno, ok := syscallErr.Err.(syscall.Errno); ok && errno == syscall.EACCES {
+					// Permission denied - port is available but requires elevated privileges
+					// Docker will handle binding with appropriate privileges
+					return nil
+				}
+			}
+		}
 		return fmt.Errorf("port %d is already in use", port)
 	}
 	listener.Close()
