e7febab83ef04b16f4f6974fd710b094899860ac
Jason Lee <lwlee2608@gmail.com>
2026-02-01 20:42:51 +0800

    v0.1.6

--- internal/config/manager.go
+++ internal/config/manager.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	DefaultVersion    = "0.1.5"
-	DefaultImageTag   = "0.1.7"
+	DefaultVersion    = "0.1.6"
+	DefaultImageTag   = "0.1.8"
 	DefaultPort       = 80
 	DefaultLLMBaseURL = "htpp://host.docker.internal:30000/v1"
 	DefaultModel      = "glm47-awq"
